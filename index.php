1f33cbbb8dea3f9f38e85e797be88e1f23b747cd
pee <pee445526@gmail.com>
2021-02-19 20:09:49 +0700

    V1 RESUME

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RESUME</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400&display=swap" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:500,900,100,300,700,400' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+    <header>
+        <div class="hd-img">
+         <img src="https://static1.squarespace.com/static/5d34e9ecbc90f800012a3f67/t/5d34ea46888c0100012e6df0/1583711601174/?format=1500w" alt="">
+        </div>
+    </header>
+
+    <div class="container-fluid">
+        <section style="background: #f1c40f; color: rgba(0, 0, 0, 0.5);">
+            <h2>RESUME</h2>
+            <nav class="shift">
+                <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </section>  
+    </div>
+
+    <br><br>
+
+    <section>
+        <div class="container c-1">
+            <div class="row">
+
+                 <div class="col-6">
+                    <div class="img-c-1 py-5">
+                    <img src="https://images.unsplash.com/photo-1609062111394-0427aa22d6c1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1976&q=80" alt="">
+                    </div>
+                 </div>
+
+                 <div class=" col-6 d-flex align-content-center flex-wrap">
+                    <div class="img-c-1 py-5">
+                    <p class="">ENVELOPES - Hand addressed envelopes for your special event never go unnoticed. In this age of emails and texts a calligraphed envelope feels personal and special, and is often saved as a memento for your special occasion.</p>
+                    </div>
+                </div>           
+            </div>
+            </div>
+    </section>
+
+    <br><br>
+
+    <section>
+        <div class="container c-2">
+            <div class="row">
+
+            <div class=" col-6 d-flex align-content-center flex-wrap">
+                    <div class="img-c-2 py-5">
+                    <p class="">ENVELOPES - Hand addressed envelopes for your special event never go unnoticed. In this age of emails and texts a calligraphed envelope feels personal and special, and is often saved as a memento for your special occasion.</p>
+                    </div>
+                </div>           
+
+
+                 <div class="col-6 col-c2">
+                    <div class="img-c-2 py-5">
+                    <img src="https://images.unsplash.com/photo-1609062111394-0427aa22d6c1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1976&q=80" alt="">
+                    </div>
+                 </div>
+
+               
+            </div>
+            </div>
+    </section>
+    <br><br>
+    <footer>
+        <div class="container-fluid">
+            <p>&copy: Copyright 2020. ALL RIGHT RESERVED MYSHOP.</p>
+        </div>
+    </footer>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+
+
+    
+
+</body>
+</html>
